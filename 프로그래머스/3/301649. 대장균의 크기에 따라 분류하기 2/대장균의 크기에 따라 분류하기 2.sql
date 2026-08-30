66ae1f2666b7920efa49d2496d06d9809f3c0f20
heeyeon-yang <yyheeyeon@gmail.com>
2026-08-30 21:59:51 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,17 @@
+-- 코드를 작성해주세요
+SELECT 
+    ID, 
+    CASE N_RANK
+        WHEN 1 THEN 'CRITICAL'
+        WHEN 2 THEN 'HIGH'
+        WHEN 3 THEN 'MEDIUM'
+        WHEN 4 THEN 'LOW'
+    END AS COLONY_NAME
+FROM (
+    SELECT
+        ID,
+        NTILE(4) OVER (ORDER BY SIZE_OF_COLONY DESC) AS N_RANK
+     FROM ECOLI_DATA
+    ) AS SUB
+
+ORDER BY ID;
