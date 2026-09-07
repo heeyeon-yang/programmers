627980311ab0f3e07cca5b452db57e0d38e52504
heeyeon-yang <yyheeyeon@gmail.com>
2026-09-07 14:28:19 +0900

    [level 0] Title: 배열 만들기 2, Time: 197.49 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,8 @@
+def solution(l, r):
+    answer = []
+    
+    for num in range(l, r+1):
+        if set(str(num)) <= {'0', '5'}:
+            answer.append(num)
+            
+    return answer if answer else [-1]
