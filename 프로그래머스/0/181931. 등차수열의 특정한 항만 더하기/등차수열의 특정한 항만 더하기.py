7350d4004eb04b318ae61bc198455127af38eff3
heeyeon-yang <yyheeyeon@gmail.com>
2026-09-16 12:24:29 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -0,0 +1,8 @@
+def solution(a, d, included):
+    answer = 0
+    
+    for i in range(len(included)):
+        if included[i] is True:
+            answer += a + d * (i)
+            
+    return answer
