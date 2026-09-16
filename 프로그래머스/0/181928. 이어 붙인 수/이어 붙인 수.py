5472fbc0dda8cf95830c46c969ad066695f27f59
heeyeon-yang <yyheeyeon@gmail.com>
2026-09-16 11:16:31 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    odd, even = '', ''
+    
+    for num in num_list:
+        if num % 2 == 0:
+            even += str(num)
+        else:
+            odd += str(num)
+    
+    return int(even) + int(odd)
